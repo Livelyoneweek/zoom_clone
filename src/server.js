d149458c0a4a093f41012380600197d71347ef56
Livelyoneweek <wlvvwhs@naver.com>
2023-01-25 23:23:11 +0900

    230125 websocket

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
+import http from "http";
+import WebSocket from 'ws';
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug")
-app.set("views",__dirname + "/src/views");
+app.set("views",__dirname + "/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (req,res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000);
+
+// app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+// server는 꼭 써야되는거아님, websocket 서버만 쓰려면 wss 사용해도 될듯함
+server.listen(3000,handleListen);
+
